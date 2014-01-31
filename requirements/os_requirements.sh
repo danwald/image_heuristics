302b3fd31f14fe6650c08e86c83049675d9e1948
danny crasto <danwald79@gmail.com>
2014-02-01 02:05:21 +0400

    added python dev to os-reqs

--- requirements/os_requirements.sh
+++ requirements/os_requirements.sh
@@ -6,6 +6,6 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 sudo apt-get update --quiet > /dev/null
-sudo apt-get install -y build-essential python-setuptools liblapack-dev liblapack3 liblapack3gf libgtk2.0-dev gfortran libblas3gf	liblapack3gf libopencv-dev libopencv-highgui-dev libcvaux-dev python-opencv python-setuptools python-virtualenv
+sudo apt-get install -y build-essential python-dev python-setuptools liblapack-dev liblapack3 liblapack3gf libgtk2.0-dev gfortran libblas3gf	liblapack3gf libopencv-dev libopencv-highgui-dev libcvaux-dev python-opencv python-setuptools python-virtualenv
 
  
