3e4a564c510e757b646fecf6ec1cb6badbc6dc3e
danny crasto <danwald79@gmail.com>
2014-02-13 08:01:36 +0400

    removed decorator for another day

--- ih/ocv_image.py
+++ ih/ocv_image.py
@@ -1,3 +1,5 @@
+import inspect
+
 import cv2
 
 from image import ImageInterface
@@ -5,21 +7,14 @@ from ops import QualityOpsInterface
 
 
 class OpenCVImage(ImageInterface, QualityOpsInterface):
-    def _loaded(func):
-        def inner(self, *args, **kwargs):
-            if not self.image:
-                self.load_image()
-            func(self, *args, **kwargs)
-        return inner
-
     def __init__(self, filename):
-        self.filename = filename
-        self.image = None
+       self.filename = filename
+       self.image = None
 
     def load_image(self):
-        self.image = cv2.imread(self.filename, cv2.IMREAD_ANYCOLOR)
+        print "loading %s"%(self.filename)
+        self.image = cv2.imread(self.filename, -1)
  
-    @_loaded
     def save_image(self, filename=''):
         cv2.imwrite(filename if filename and len(filename) else self.filename, self.image)
 
